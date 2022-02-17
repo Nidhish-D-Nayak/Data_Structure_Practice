794ac8cf050c78aeea976bb23ae52d8b21f27b74
Nidhish <nidhibelthangady@gmail.com>
2022-02-17 13:21:12 +0530

    Added a DS_arrays file to repo

---
+++ DS_arrays.js
@@ -0,0 +1,27 @@
+//#1 BASIC ARRAYS - Static Arrays
+console.log("Hello world");
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    get(index) {
+        return this.data[index];
+    }
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.legth;
+    }
+}
+const newArray = new MyArray();
+newArray.push("Hi");
+console.log(newArray);
+
+// #HashMaps Example
+let hashMapMain = new Map();
+hashMapMain.set("main", "project");
+hashMapMain.set("flag", "projec2");
+console.log(hashMapMain);
+
+// #
